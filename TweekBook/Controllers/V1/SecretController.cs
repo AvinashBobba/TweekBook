4c7ef30342abf40887c29b452cb5798fa59e4024
Avinash Bobba <42165272+AvinashBobba@users.noreply.github.com>
2020-08-08 18:39:39 +0100

    #AB : API Key Authentication

---
+++ TweekBook/Controllers/V1/SecretController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TweekBook.Filters;
+
+namespace TweekBook.Controllers.V1
+{
+    [ApiKeyAuth]
+    public class SecretController : ControllerBase
+    {
+        [HttpGet("secret")]
+        public IActionResult GetSecret()
+        {
+            return Ok("I Have No Secrets");
+        }
+    }
+}
--- TweekBook/Filters/ValidationFilter.cs
+++ TweekBook/Filters/ValidationFilter.cs
@@ -40,8 +40,6 @@ namespace TweekBook.Filters
             }
 
             await next();
-
-            //after controller
         }
     }
 }
